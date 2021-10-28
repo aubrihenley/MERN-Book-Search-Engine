ab21f60d04429a6cd66423016d5cb453b1fbbb1f
Aubri Henley <aubri.henley@gmail.com>
2021-10-28 12:06:34 -0400

    created typeDefs in Schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  input Book {
+    bookID: Int
+    authors: [String]!
+    description: String
+    title: String
+    media: [MediaDetails!]
+    link: String
+  }
+
+  input MediaDetails {
+      format: MediaFormat!
+      url: String!
+  }
+
+  enum MediaFormat{
+      IMAGE
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(content: Book!): User
+    removeBook(bookId: Int): User
+  }
