16d30344f5a3ff7be961ac41af59ec18e2893242
Aubri Henley <aubri.henley@gmail.com>
2021-10-29 17:53:55 -0400

    added query to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,24 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { Book, User } = require("../models");
 const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find({});
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("savedBooks");
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("savedBooks");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutations: {},
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
-console.log(authMiddleware);
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
